d6f0bd82f901630a17d607cdd0d43db3324ae511
kemeng <929121977@qq.com>
2021-11-26 18:27:25 +0800

    fix

--- logs/kvs.go
+++ logs/kvs.go
@@ -1,1 +1,27 @@
 package logs
+
+import (
+	"context"
+	"sync"
+)
+
+type KeyVal struct {
+	key interface{}
+	val interface{}
+}
+
+type LogField struct {
+	kvs       []KeyVal
+	fieldLock sync.RWMutex
+}
+
+type kvsIdKey struct{}
+
+func getFields(ctx context.Context) *LogField {
+	field, ok := ctx.Value(kvsIdKey{}).(*LogField)
+	if !ok {
+		return nil
+	}
+
+	return field
+}
